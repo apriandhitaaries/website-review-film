bd486febd50f4b9d105a29befdc1cdfd388d8822
apriandhitaaries <apriandhitaaries@gmail.com>
2025-11-10 16:26:32 +0700

    Setup database schema with all migrations

---
+++ database/migrations/2025_11_06_081909_create_films_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul', length: 255);
+            $table->text('ringkasan');
+            $table->year('tahun');
+            $table->string('poster', 255);
+
+            $table->foreignId('genre_id')
+                ->constrained('genres');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
---
+++ database/migrations/2025_11_06_082355_create_cast_film_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cast_film', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('peran', 255);
+
+            $table->foreignId('cast_id')
+                ->constrained('casts');
+
+            $table->foreignId('film_id')
+                ->constrained('films');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cast_film');
+    }
+};
